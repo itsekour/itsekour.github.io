05c21d1c5c8cfbfd216b598e9e9c959b36c5131d
Iraklis Tsekourakis <itsekour@stevens.edu>
2017-12-29 15:09:02 +0200

    first version done

--- tmp/js/script.js
+++ tmp/js/script.js
@@ -1,17 +1,31 @@
 window.addEventListener("load", function() {
 
-
-
     var menuBtn = document.getElementById("menu-btn");
+    var nav = document.querySelector("nav");
+    var node = document.getElementById("profPic");
+    var navContainer = document.getElementById("navContainer");
 
     menuBtn.addEventListener("click", function (e) {
+        if (window.innerWidth<1024) {
+            node.classList.toggle("hide");
+            node.classList.toggle("profile-picture");
+            nav.classList.toggle("show");
+            navContainer.classList.toggle("nav-expand");
 
-        var node = document.getElementById("profPic");
-        node.classList.toggle("hide");
-        node.classList.toggle("profile-picture");
-
+        }
     });
 
+
+    nav.addEventListener("click", function (e) {
+
+       if (window.innerWidth<1024) {  
+            node.classList.toggle("hide");
+            node.classList.toggle("profile-picture");
+            nav.classList.toggle("show");
+            navContainer.classList.toggle("nav-expand");
+        }
+
+    });
 });
 
 
